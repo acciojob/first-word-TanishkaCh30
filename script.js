a2c91ca41cee47742d338c0d821a3cbb189662ff
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-04-03 22:11:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function firstWord(s) {
 s = s.trim(); // Remove leading and trailing spaces
     let index = s.indexOf(" "); // Find the first space
     return index === -1 ? s : s.slice(0, index);
-	
+}
 
 
-// Do not change the code below
+// Do not change the code below;
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
